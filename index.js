5b9fd5fa929319ee5dd3ca131ca915a74b3e234e
ChaosInventor <chaosinventor@yandex.com>
2023-12-31 22:06:25 +0100

    Get canvases

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("Game script start")
+
+const friendlyCanvas = document.getElementById("friendly-canvas")
+const neutralCanvas = document.getElementById("neutral-canvas")
+const enemyCanvas = document.getElementById("enemy-canvas")
+
