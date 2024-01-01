5e0721611d8a63b622518cc4cb7671a528400c44
ChaosInventor <chaosinventor@yandex.com>
2024-01-01 13:13:22 +0100

    Handle canvas resizing

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ const friendlyCanvas = document.getElementById("friendly-canvas")
 const neutralCanvas = document.getElementById("neutral-canvas")
 const enemyCanvas = document.getElementById("enemy-canvas")
 
+function updateCanvasSize(canvas) {
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+}
+
+const resizeObserver = new ResizeObserver((entries) => {
+    for(const entry of entries) {
+        updateCanvasSize(entry.target);
+    }
+});
+
+resizeObserver.observe(friendlyCanvas);
+resizeObserver.observe(neutralCanvas);
+resizeObserver.observe(enemyCanvas);
