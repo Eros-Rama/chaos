5a1aa4d6851dbba8cf6e793cc29602e782f790ce
ChaosInventor <chaosinventor@yandex.com>
2024-01-01 16:35:46 +0100

    Add grid drawing

--- index.js
+++ index.js
@@ -15,6 +15,58 @@ const resizeObserver = new ResizeObserver((entries) => {
     }
 });
 
-resizeObserver.observe(friendlyCanvas);
-resizeObserver.observe(neutralCanvas);
-resizeObserver.observe(enemyCanvas);
+resizeObserver.observe(friendlyCanvas)
+resizeObserver.observe(neutralCanvas)
+resizeObserver.observe(enemyCanvas)
+
+function drawGridClip(ctx, grid) {
+    ctx.fillStyle = grid.background
+    ctx.strokeStyle = grid.foreground
+
+    //Grid bounds
+    ctx.fillRect(grid.x, grid.y, grid.width, grid.height)
+    ctx.strokeRect(grid.x, grid.y, grid.width, grid.height)
+
+    ctx.beginPath()
+
+    //Rows
+    const rowWidth = grid.width / grid.rows * grid.scale
+    for(let row = 0; row <= grid.rows; row++)
+    {
+        const sx = grid.x + (row * rowWidth) + grid.offset
+        const sy = grid.y + grid.offset
+        const ex = sx
+        const ey = Math.min(sy + grid.height, grid.height)
+        if(sx > grid.width || sy > grid.height)
+        {
+            break
+        }
+
+        ctx.moveTo(sx, sy)
+        ctx.lineTo(ex, ey)
+    }
+
+    //Columns
+    const columnHeight = grid.width / grid.columns * grid.scale
+    for(let column = 0; column <= grid.columns; column++)
+    {
+        const sx = grid.x + grid.offset
+        const sy = grid.y + (column * columnHeight) + grid.offset
+        const ex = Math.min(sx + grid.width, grid.width)
+        const ey = sy
+        if(sx > grid.width || sy > grid.height)
+        {
+            break
+        }
+
+        ctx.moveTo(sx, sy)
+        ctx.lineTo(ex, ey)
+    }
+
+    ctx.closePath()
+    ctx.stroke()
+
+}
+
+const ctx = enemyCanvas.getContext("2d")
+setTimeout(() => drawGridClip(ctx, {x: 0, y: 0, width: 100, height: 100, foreground: "black", background: "white", rows: 10, columns: 10, offset: 0, scale: 1}), 500)
