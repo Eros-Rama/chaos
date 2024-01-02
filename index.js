aac8280750bddcbf62734a27c7b5c04b6d610b2e
ChaosInventor <chaosinventor@yandex.com>
2024-01-02 16:47:20 +0100

    Add grid dragging
    Also includes fixes to edge cases with rendering.

--- index.js
+++ index.js
@@ -28,37 +28,49 @@ function drawGridClip(ctx, grid) {
     ctx.fillRect(grid.x, grid.y, grid.width, grid.height)
     ctx.strokeRect(grid.x, grid.y, grid.width, grid.height)
 
+    //Derived grid parameters
+    const startX = grid.x + grid.offsetX
+    const startY = grid.y + grid.offsetY
+
+    const rowWidth = grid.width / grid.rows * grid.scale
+    const columnHeight = grid.width / grid.columns * grid.scale
+
+    const gridTrueWidth = rowWidth * grid.rows
+    const gridTrueHeight = columnHeight * grid.columns
+
+    const gridMaxX = grid.x + grid.width
+    const gridMaxY = grid.y + grid.height
+
+    //Drawing
     ctx.beginPath()
 
-    //Rows
-    const rowWidth = grid.width / grid.rows * grid.scale
     for(let row = 0; row <= grid.rows; row++)
     {
-        const sx = grid.x + (row * rowWidth) + grid.offset
-        const sy = grid.y + grid.offset
+        const sx = startX + (row * rowWidth)
+        if(sx < grid.x) continue
+
+        const sy = Math.min(Math.max(startY, grid.y), gridMaxY)
+
         const ex = sx
-        const ey = Math.min(sy + grid.height, grid.height * grid.scale)
-        if(sx > grid.width || sy > grid.height)
-        {
-            break
-        }
+        const ey = Math.max(Math.min(startY + gridTrueHeight, gridMaxY), grid.y)
+
+        if(ex > gridMaxX || ey > gridMaxY) continue
 
         ctx.moveTo(sx, sy)
         ctx.lineTo(ex, ey)
     }
 
-    //Columns
-    const columnHeight = grid.width / grid.columns * grid.scale
     for(let column = 0; column <= grid.columns; column++)
     {
-        const sx = grid.x + grid.offset
-        const sy = grid.y + (column * columnHeight) + grid.offset
-        const ex = Math.min(sx + grid.width, grid.width * grid.scale)
+        const sx = Math.min(Math.max(startX, grid.x), gridMaxY)
+
+        const sy = startY + (column * columnHeight)
+        if(sy < grid.y) continue
+
+        const ex = Math.max(Math.min(startX + gridTrueWidth, gridMaxX), grid.x)
         const ey = sy
-        if(sx > grid.width || sy > grid.height)
-        {
-            break
-        }
+
+        if(ex > gridMaxX || ey > gridMaxY) continue
 
         ctx.moveTo(sx, sy)
         ctx.lineTo(ex, ey)
@@ -72,22 +84,37 @@ let testGrid = {
     x: 0, y: 0, width: 100, height: 100,
     foreground: "black", background: "white",
     rows: 10, columns: 10,
-    offset: 0, scale: 1,
-    scrollFactor: 0.001
+    offsetX: 0, offsetY: 0, scale: 1,
+    scrollFactor: 0.001, offsetFactor: 1
 }
 const ctx = enemyCanvas.getContext("2d")
 function render() {
-    testGrid.width = enemyCanvas.width
-    testGrid.height = enemyCanvas.height
+    testGrid.width = enemyCanvas.width / 2
+    testGrid.height = enemyCanvas.height / 2
+    testGrid.x = enemyCanvas.width / 4
+    testGrid.y = enemyCanvas.height / 4
     drawGridClip(ctx, testGrid)
 }
 
 function scaleGrid(e) {
     e.preventDefault()
 
-    testGrid.scale += e.deltaY * testGrid.scrollFactor
+    testGrid.scale -= e.deltaY * testGrid.scrollFactor
     render()
 }
 
-console.log('adding event listener')
+let mouseDown = false;
+function offsetGrid(e) {
+    e.preventDefault()
+
+    if(mouseDown) {
+        testGrid.offsetX += e.movementX * testGrid.offsetFactor
+        testGrid.offsetY += e.movementY * testGrid.offsetFactor
+        render()
+    }
+}
+
 enemyCanvas.addEventListener("wheel", scaleGrid, { passive: false })
+enemyCanvas.addEventListener("mousemove", offsetGrid, { passive: false })
+enemyCanvas.addEventListener("mousedown", (e) => mouseDown = true, { passive: true })
+enemyCanvas.addEventListener("mouseup", (e) => mouseDown = false, { passive: true })
